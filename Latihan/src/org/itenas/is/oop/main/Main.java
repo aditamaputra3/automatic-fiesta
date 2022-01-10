c0ce0a1c412be3c02a01e345aab6c5e696030628
Adi <Adi@fedora>
2022-01-10 07:39:35 +0700

    ssss

---
+++ Latihan/src/org/itenas/is/oop/main/Main.java
@@ -0,0 +1,7 @@
+package org.itenas.is.oop.main;
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ Latihan/src/org/itenas/is/oop/model/Car.java
@@ -0,0 +1,27 @@
+package org.itenas.is.oop.model;
+
+public class Car {
+	private String merk;
+	private String color;
+	private String factory;
+	private String doors;
+	private String engine;
+	private String driver;
+	private float speed;
+	private double price;
+	private int usage;
+	
+	public Car(String merk, String color, String factory, String doors, String engine, String driver, float speed, double price, int usage) {
+		this.merk = merk;
+		this.color = color;
+		this.factory = factory;
+		this.doors = doors;
+		this.engine = engine;
+		this.driver = driver;
+		this.speed = speed;
+		this.price = price;
+		this.usage = usage;
+		
+		
+	}
+}
